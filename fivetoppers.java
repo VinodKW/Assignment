71a50b3bf257458d222e19cd8e4c41b4498672d0
VinodKW <40213599+vkw1111@users.noreply.github.com>
2019-08-09 22:01:29 +0530

    Update fivetoppers.java
    Comments added

--- fivetoppers.java
+++ fivetoppers.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 class Toppers implements Comparator<StudentData> {
 
-    public int compare(StudentData d1, StudentData d2) {
-    	Integer I1 = (Integer)d1.marks;
-    	Integer I2 = (Integer)d2.marks;
+    // for Sorting of student data 
+        public int compare(StudentData d1, StudentData d2) {
+    	    Integer I1 = (Integer)d1.marks;
+    	    Integer I2 = (Integer)d2.marks;
 
-    	return I2 - I1;
-    }
+    	    return I2 - I1;
+        }
 
 	public static void main(String[] args) throws IOException {
 
@@ -17,40 +18,42 @@ class Toppers implements Comparator<StudentData> {
 
         BufferedReader br = new BufferedReader(new FileReader( new File("file1.txt")));
 
-		String s = br.readLine();
+        String s = br.readLine();
         
         List<StudentData> student = new ArrayList<StudentData>();
 
+        //for fetching out the data in txt/csv file and saving its pointer value in the reference br1
         BufferedReader br1 = new BufferedReader(new FileReader( new File("file1.txt")));
 
         s = br1.readLine();
         int k = 0;
-
-        while( s!=null ) {
-        	String[] temp = s.split(",");
-        	student.add(new StudentData( Integer.parseInt(temp[0]), temp[1], Integer.parseInt(temp[2]), temp[3], temp[4]));
+        
+		
+	//storing the student data in form of list variable student
+        while ( s!=null ) {
+            String[] temp = s.split(",");
+            student.add(new StudentData( Integer.parseInt(temp[0]), temp[1], Integer.parseInt(temp[2]), temp[3], temp[4]));
             s = br1.readLine();
         }
+        
         br1.close();
 
-
+        //Sorting in descending order according to marks
         Collections.sort(student, t);
         
         System.out.println("List of top 5 toppers");
         int count = 0;
 
-        for(StudentData std: student) {
+        //Displaying to top 5 students according to marks
+        for (StudentData std: student) {
         	if(count == 5) {
         		break;
         	} else {
         		System.out.println("Rollno:"+std.rollno+" Name:"+std.name+" Marks:"+std.marks+" Branch:"+std.branch+" Institute:"+std.institute);
         		count++;
         	}
-        	
         }
-
-
-	}
+    }
 }
 
 class StudentData {
